7a13a0e9acb391927ca1d7798bc6b7836bc6f8ba
martin-gk <you@example.comguevara.mrtn@gmail.com>
2021-01-30 19:54:36 +0100

    added button to join and create pages

--- lib/ui/group.dart
+++ lib/ui/group.dart
@@ -3,16 +3,51 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(Group());
+class OtpTimer extends StatefulWidget {
+  @override
+  _OtpTimerState createState() => _OtpTimerState();
+}
 
-class Group extends StatelessWidget {
+class _OtpTimerState extends State<OtpTimer> {
+  final interval = const Duration(seconds: 1);
+
+  final int timerMaxSeconds = 1500;
+
+  int currentSeconds = 0;
+
+  String get timerText =>
+      '${((timerMaxSeconds - currentSeconds) ~/ 60).toString().padLeft(2, '0')}: ${((timerMaxSeconds - currentSeconds) % 60).toString().padLeft(2, '0')}';
+
+  startTimeout([int milliseconds]) {
+    var duration = interval;
+    Timer.periodic(duration, (timer) {
+      setState(() {
+        print(timer.tick);
+        currentSeconds = timer.tick;
+        if (timer.tick >= timerMaxSeconds) timer.cancel();
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    startTimeout();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return
-    Center(
-          child: Text('Hello World'),
-
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(Icons.timer),
+        SizedBox(
+          width: 5,
+        ),
+        Text(timerText)
+      ],
     );
   }
 }
