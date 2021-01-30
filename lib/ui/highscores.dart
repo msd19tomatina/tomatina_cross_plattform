b0afd96ba8cc87702d090cfefebbc3ccc0ba591f
martin-gk <you@example.comguevara.mrtn@gmail.com>
2021-01-30 16:28:59 +0100

    added previously ignored files

---
+++ lib/ui/highscores.dart
@@ -0,0 +1,17 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class Highscores extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Center(
+          child: Text('Highscores'),
+
+
+    );
+  }
+}
---
+++ lib/ui/joinGroup.dart
@@ -0,0 +1,16 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class JoinGroup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+   Center(
+          child: Text('Join Group'),
+
+    );
+  }
+}
